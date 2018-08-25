3ebaad2ae59a7e4ed80685a0889094dbf14732ab
Shireesha Bongarala <bongaralashireesha@gmail.com>
2018-08-25 22:31:37 +0530

    Add simple server

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const handleRequest = (request, response) => {
+ console.log('Received request for URL: ' + request.url);
+ response.writeHead(200);
+ response.end('Hello World!');
+};
+const www = http.createServer(handleRequest);
+www.listen(8080);
